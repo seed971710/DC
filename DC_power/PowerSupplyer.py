6f88c9bf5352c41384587ab67b1bfb9886bf25f7
Ethan_Jian <guanyian.jian@xsquareiot.com>
2023-04-11 10:17:32 +0800

    v0

---
+++ DC_power/PowerSupplyer.py
@@ -0,0 +1,109 @@
+import pyvisa
+
+class PowerSupply:
+
+    def __init__(self):
+        
+        # Initialize PyVISA resource manager
+        self.rm = pyvisa.ResourceManager()
+        self.GPIB = [value for value in self.rm.list_resources() if 'GPIB0::' in value]
+        # Open the power supply
+        self.inst = self.rm.open_resource(self.GPIB[0])
+        # Set termination characters
+        self.inst.read_termination = '\n'
+        self.inst.write_termination = '\n'
+
+
+    def __del__(self):
+        # Close the instrument when the object is deleted
+        self.inst.close()
+
+    #--- public methods ---
+
+    @property        
+    def voltage(self):
+        """
+        Returns the voltage setting
+        """
+        return float(self.inst.query('VOLTage:LEVel?'))
+
+    @voltage.setter
+    def voltage(self, volts):
+        """
+        Sets the voltage of the instrument
+        """
+        self.inst.write(f'VOLTage:LEVel {volts}')
+
+    @property
+    def current(self):
+        """
+        Returns the current setting in Amps
+        """
+        return float(self.inst.query('CURRent:LEVel?'))
+
+    @current.setter
+    def current(self, amps):
+        """
+        Set the current of the instrument
+        """
+        self.inst.write(f'CURRent:LEVel {amps}')
+    
+    @property
+    def CURR_PROT(self):
+        """
+        Returns the protect current setting in Amps
+        """
+        return bool(int(self.inst.query('CURRent:PROTection:STATe?')))
+
+    @CURR_PROT.setter
+    def CURR_PROT(self, state):
+        """
+        Set the protection current of the instrument
+        """
+        if state:
+            self.inst.write('CURRent:PROTection:STATe ON')
+        else:
+            self.inst.write('CURRent:PROTection:STATe OFF')
+    
+    @property
+    def CURR_PROT_VALUE(self):
+        """
+        Returns the protect current setting in Amps
+        """
+        return float(self.inst.query('CURRent:PROTection:STATe?'))
+
+
+    @CURR_PROT_VALUE.setter
+    def CURR_PROT_VALUE(self, amps):
+        """
+        Set the protection current of in Amps
+        """
+        self.inst.write(f'CURRent:PROTection {amps}')
+
+
+    def on(self):
+        """
+        Turns the output on
+        """
+        self.inst.write('OUTPut:STATe ON')
+
+    def off(self):
+        """
+        Turns the output off
+        """
+        self.inst.write('OUTPut:STATe OFF')
+        self.inst.write('CURRent:PROTection:STATe OFF')
+
+    def read_current(self):
+        """
+        Returns the measured current in Amps
+        """
+        return float(self.inst.query('MEASure:CURRent?'))
+    
+    def read_voltage(self):
+
+        """
+        Returns the measured current in Amps
+        """
+        return float(self.inst.query('MEASure:VOLTage:DC?'))
+    
